eb7355dd18cbefd18dd05a3205b9065efc5e6722
SifaKasena <sifacollins@gmail.com>
2023-01-24 12:11:18 +0300

    Handle %, c, s modifiers

--- printf.c
+++ printf.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <unistd.h>
+#include <stdarg.h>
+#include <string.h>
 
 /**
  * _printf - custom printf function
@@ -11,11 +13,36 @@
 int _printf(const char *format, ...)
 {
 	unsigned int i = 0;
+	va_list ptr;
+	char c, *s;
 
+	va_start(ptr, format);
 	while (format[i] != '\0')
 	{
-		write(1, format + i, 1);
+		if (format[i] != '%')
+			write(1, format + i, 1);
+		else
+		{
+			i++;
+			switch (format[i])
+			{
+				case '%':
+					write(1, format + i, 1);
+					break;
+				case 'c':
+					c = va_arg(ptr, int);
+					write(1, &c, 1);
+					break;
+				case 's':
+					s = va_arg(ptr, char *);
+					write(1, s, strlen(s) + 1);
+					break;
+				default:
+					break;
+			}
+		}
 		i++;
 	}
+	va_end(ptr);
 	return (0);
 }
